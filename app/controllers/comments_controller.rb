a6fabb1b807fe8946d2bafdc40dd7ba1e362aa26
celestegerard <celeste.gerard@gmail.com>
2020-08-20 17:28:51 -0400

    added delete comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,13 @@ class CommentsController < ApplicationController
     redirect_to prize_path(@prize)
   end
 
+  def destroy
+    @prize = Prize.find(params[:prize_id])
+    @comment = @prize.comments.find(params[:id])
+    @comment.destroy
+    redirect_to prize_path(@prize)
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:commenter, :body)
