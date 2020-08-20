bf2c569ed40ca3dc2b5e817011415ad37ad5f449
celestegerard <celeste.gerard@gmail.com>
2020-08-20 12:07:20 -0400

    added model and db; migrated

--- app/controllers/prizes_controller.rb
+++ app/controllers/prizes_controller.rb
@@ -3,4 +3,17 @@ class PrizesController < ApplicationController
   def new
   end
 
+  def create
+    
+    @prize = Prize.new(prize_params)
+
+  @prize.save
+  redirect_to @prize
+  end
+
+  private
+  def prize_params
+    params.require(:prize).permit(:title, :text)
+  end
+
 end
