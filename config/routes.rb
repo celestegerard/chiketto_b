5e6738e0da25b11eed3a938f0ff1f92e7a59fe25
celestegerard <celeste.gerard@gmail.com>
2020-08-21 15:04:28 -0400

    prepped for api

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
+
   get 'welcome/index'
+  root 'welcome#index'
 
   resources :prizes do
     resources :comments
   end
 
-
-  root 'welcome#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
